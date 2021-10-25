60c3bca050d1a0de67c1a9aa95d2843fceaecac3
guvi-b27gwd <93111419+guvi-b27gwd@users.noreply.github.com>
2021-10-25 10:01:52 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to Guvi B27G WD</p>
+        <p>Welcome to Guvi B27G WD - Zen Classes</p>
       </header>
     </div>
   );
